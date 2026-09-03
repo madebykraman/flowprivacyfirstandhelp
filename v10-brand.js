5283ad58b4a9a6790725cb57bf3f996111701298
madebykraman <madebykraman@gmail.com>
2026-09-04 04:58:25 +0530

    chore: remove obsolete Ritva brand shim

--- v10-brand.js
+++
@@ -1,9 +0,0 @@
-/* Ritva V0.10 brand layer. Presentation only. */
-(function(){
-  const swap=node=>{if(node.nodeType===Node.TEXT_NODE){if(node.nodeValue.includes('NijRitu'))node.nodeValue=node.nodeValue.replaceAll('NijRitu','Ritva');if(node.nodeValue.includes('निजऋतु'))node.nodeValue=node.nodeValue.replaceAll('निजऋतु','Ritva')}else if(node.nodeType===Node.ELEMENT_NODE){for(const child of node.childNodes)swap(child)}};
-  function apply(){document.title=document.title.replaceAll('NijRitu','Ritva');swap(document.getElementById('app'));swap(document.querySelector('.topbar'))}
-  const app=document.getElementById('app');
-  if(app)new MutationObserver(apply).observe(app,{childList:true,subtree:true,characterData:true});
-  new MutationObserver(apply).observe(document.documentElement,{childList:true,subtree:true,characterData:true});
-  apply();
-})();
